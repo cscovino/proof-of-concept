7a78703804f5cec901e0102b785589db7efee47d
Carlos <kikescovino@gmail.com>
2020-02-08 18:21:13 -0300

    dynamic resizes api GO

--- thumbnail-generator-api/main.go
+++ thumbnail-generator-api/main.go
@@ -3,24 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"image"
 	"image/jpeg"
 	"image/png"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/nfnt/resize"
 )
-
-type response struct {
-	Orig string `json:"orig"`
-	R400 string `json:"r400"`
-	R160 string `json:"r160"`
-	R120 string `json:"r120"`
-}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
@@ -36,7 +32,8 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close() // Close the file when we finish
 
-	name := strings.Split(handler.Filename, ".")
+	var name []string = strings.Split(handler.Filename, ".")
+	var ext string = name[len(name)-1]
 
 	if strings.ToLower(name[len(name)-1]) != "png" && strings.ToLower(name[len(name)-1]) != "jpg" && strings.ToLower(name[len(name)-1]) != "jpeg" {
 		fmt.Fprintf(w, "File extension should be png or jpg")
@@ -47,102 +44,61 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "File should be less than 5MB")
 		return
 	}
+	var sizes []string = strings.Split(os.Getenv("SIZES"), ",")
+	var resp string = resizeImage(ext, sizes, name, &file, r.Host)
 
-	var ext string
-
-	if strings.ToLower(name[len(name)-1]) == "png" {
-		img, errDecode := png.Decode(file)
-		if errDecode != nil {
-			fmt.Fprintf(w, "Bad Image")
-		}
-
-		out, errFile := os.Create("images/" + name[0] + ".png")
-		if errFile != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out.Close()
-		// write new image to file
-		png.Encode(out, img)
-
-		img400 := resize.Resize(400, 300, img, resize.Lanczos3)
-		out400, errFile400 := os.Create("images/" + name[0] + "_400x300.png")
-		if errFile400 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out400.Close()
-		// write new image to file
-		png.Encode(out400, img400)
-
-		img160 := resize.Resize(160, 120, img, resize.Lanczos3)
-		out160, errFile160 := os.Create("images/" + name[0] + "_160x120.png")
-		if errFile160 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out160.Close()
-		// write new image to file
-		png.Encode(out160, img160)
-
-		img120 := resize.Resize(120, 120, img, resize.Lanczos3)
-		out120, errFile120 := os.Create("images/" + name[0] + "_120x120.png")
-		if errFile120 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out120.Close()
-		// write new image to file
-		png.Encode(out120, img120)
-
-		ext = "png"
-	} else {
-		img, errDecode := jpeg.Decode(file)
-		if errDecode != nil {
-			fmt.Fprintf(w, "Bad Image")
-		}
-
-		out, errFile := os.Create("images/" + name[0] + ".jpg")
-		if errFile != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out.Close()
-		// write new image to file
-		jpeg.Encode(out, img, nil)
-
-		img400 := resize.Resize(400, 300, img, resize.Lanczos3)
-		out400, errFile400 := os.Create("images/" + name[0] + "_400x300.jpg")
-		if errFile400 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out400.Close()
-		// write new image to file
-		jpeg.Encode(out400, img400, nil)
-
-		img160 := resize.Resize(160, 120, img, resize.Lanczos3)
-		out160, errFile160 := os.Create("images/" + name[0] + "_160x120.jpg")
-		if errFile160 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out160.Close()
-		// write new image to file
-		jpeg.Encode(out160, img160, nil)
-
-		img120 := resize.Resize(120, 120, img, resize.Lanczos3)
-		out120, errFile120 := os.Create("images/" + name[0] + "_120x120.jpg")
-		if errFile120 != nil {
-			fmt.Fprintf(w, "Error creating file")
-		}
-		defer out120.Close()
-		// write new image to file
-		jpeg.Encode(out120, img120, nil)
-
-		ext = "jpg"
-	}
-
-	var resp = response{
-		Orig: r.Host + "/images/" + handler.Filename,
-		R400: r.Host + "/images/" + name[0] + "_400x300." + ext,
-		R160: r.Host + "/images/" + name[0] + "_160x120." + ext,
-		R120: r.Host + "/images/" + name[0] + "_120x120." + ext,
+	if resp == "" {
+		fmt.Fprintf(w, "Error while resizing")
+		return
 	}
 	json.NewEncoder(w).Encode(resp)
+}
+
+func resizeImage(ext string, sizes []string, name []string, file *multipart.File, host string) string {
+	var img image.Image
+	var errDecode error
+	var resp string = "{"
+
+	if ext != "png" {
+		img, errDecode = jpeg.Decode(*file)
+	} else {
+		img, errDecode = png.Decode(*file)
+	}
+	if errDecode != nil {
+		return ""
+	}
+
+	var top int = len(sizes)
+	for i := 0; i < top; i++ {
+		var dim []string = strings.Split(sizes[i], "x")
+		width, errW := strconv.Atoi(dim[0])
+		if errW != nil {
+			return ""
+		}
+		height, errH := strconv.Atoi(dim[1])
+		if errH != nil {
+			return ""
+		}
+		newImg := resize.Resize(uint(width), uint(height), img, resize.Lanczos3)
+		var path string = os.Getenv("IMAGES") + "/" + name[0] + "_" + dim[0] + "x" + dim[1] + "." + ext
+		out, errFile := os.Create(path)
+		if errFile != nil {
+			return ""
+		}
+		// write new image to file
+		if ext != "png" {
+			jpeg.Encode(out, newImg, nil)
+		} else {
+			png.Encode(out, newImg)
+		}
+		out.Close()
+		resp += `"` + sizes[i] + `":"` + host + "/" + path + `"`
+		if i < top-1 {
+			resp += ","
+		}
+	}
+	resp += "}"
+	return resp
 }
 
 func main() {
@@ -150,8 +106,8 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink).Methods("GET")
-	router.HandleFunc("/upload", uploadFile).Methods("POST")
-	router.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("./images/"))))
+	router.HandleFunc(os.Getenv("ENDPOINT"), uploadFile).Methods("POST")
+	router.PathPrefix("/" + os.Getenv("IMAGES") + "/").Handler(http.StripPrefix("/"+os.Getenv("IMAGES")+"/", http.FileServer(http.Dir("./"+os.Getenv("IMAGES")+"/"))))
 
 	headers := handlers.AllowedHeaders([]string{"Access-Control-Allow-Origin", "Content-Type"})
 	origins := handlers.AllowedOrigins([]string{"*"})
